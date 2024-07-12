06a853a2281f34a7778100b540c1dfa6818d0a08
Olaiday1 <hammedahishat7@gmail.com>
2024-07-12 23:12:26 +0100

    first commit

---
+++ card-profile/src/Card.jsx
@@ -0,0 +1,79 @@
+
+function Card() {
+    return(
+        <>
+        <div className="heading">
+            <h1>IOTB TECH STUDENTS PROFILE</h1>
+            </div>
+        <div className="card-container">
+        <div className="card">
+            <h2 className="head">Student Profile</h2>
+            <p>Name: Hammed Ahishat O.</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+        </div>
+        <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Hawau Owonikoko</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+        </div>
+        <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Misbaudeen Kehinde B.</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Oriade Latifat</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Aderolu Lateefat</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Abdulmumeen Etudaye M.</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Yunusa Usman N.</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Obayomi Abdulafeez</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+            <div className="card">
+        <h2 className="head">Student Profile</h2>
+            <p>Name: Balogun Waliyat</p>
+            <p>Course: Web Development</p>
+            <p>Duration: 6 Months</p>
+            <p>Year: 2024</p>
+            </div>
+
+            </div>
+        </>
+    );
+
+}
+
+export default Card
